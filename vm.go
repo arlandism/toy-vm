14725787617fc9e10477a11295427113a5e72f73
Arlandis Word <arlandis@remind101.com>
2021-03-15 14:59:59 -0400

    Init vm

---
+++ vm.go
@@ -0,0 +1,75 @@
+package vm
+
+import "fmt"
+
+const (
+	Load  = 0x01
+	Store = 0x02
+	Add   = 0x03
+	Sub   = 0x04
+	Halt  = 0xff
+)
+
+// Stretch goals
+const (
+	Addi = 0x05
+	Subi = 0x06
+	Jump = 0x07
+	Beqz = 0x08
+)
+
+// Given a 256 byte array of "memory", run the stored program
+// to completion, modifying the data in place to reflect the result
+//
+// The memory format is:
+//
+// 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f ... ff
+// __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ ... __
+// ^==DATA===============^ ^==INSTRUCTIONS==============^
+//
+func compute(memory []byte) {
+	registers := []byte{0x08, 0x00, 0x00}
+	pc := registers[0]
+	for {
+		op := memory[pc]
+		switch op {
+		// begin "normal" ops
+		// byte immediately after pc is a register
+		// byte after register is a memory address
+		case Load:
+			registers[memory[pc+1]] = memory[memory[pc+2]]
+		case Store:
+			memory[memory[pc+2]] = registers[memory[pc+1]]
+		case Add:
+			registers[memory[pc+1]] = registers[memory[pc+1]] + registers[memory[pc+2]]
+		case Sub:
+			registers[memory[pc+1]] = registers[memory[pc+1]] - registers[memory[pc+2]]
+			// begin "immediate" ops
+			// byte immediately after pc is a register
+			// byte after register is a value to be added/subtracted
+		case Addi:
+			registers[memory[pc+1]] = registers[memory[pc+1]] + memory[pc+2]
+		case Subi:
+			registers[memory[pc+1]] = registers[memory[pc+1]] - memory[pc+2]
+			// begin "flow control" ops
+		case Jump:
+			// byte immediately after pc is a memory address to jump to
+			// skip the normal pc increment
+			pc = memory[pc+1]
+			continue
+			// byte immediately after pc is a register to check
+			// byte after register is the memory address to jump to if reg value is 0
+		case Beqz:
+			if registers[memory[pc+1]] == 0 {
+				imm := memory[pc+2]
+				pc = pc + imm
+			}
+			// end program
+		case Halt:
+			return
+		default:
+			fmt.Println(op, "unsupported op code. skipping")
+		}
+		pc = pc + 3
+	}
+}
